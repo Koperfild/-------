3f0d9c6801fa1812948449426640ca8ff1b60bf2
Koperfild <Avalancher2@rambler.ru>
2014-06-05 01:57:53 +0300

    в
    в

---
+++ Лаба1/Лаба1/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Лаба1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.MainTable = new System.Windows.Forms.DataGridView();
+            this.Наименование = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Цена = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Количество = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ф = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.в = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.productsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.bindingNavigator1 = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MainTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).BeginInit();
+            this.bindingNavigator1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainTable
+            // 
+            this.MainTable.AutoGenerateColumns = false;
+            this.MainTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.MainTable.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Наименование,
+            this.Цена,
+            this.Количество,
+            this.ф,
+            this.в});
+            this.MainTable.DataSource = this.productsBindingSource;
+            this.MainTable.Location = new System.Drawing.Point(12, 44);
+            this.MainTable.Name = "MainTable";
+            this.MainTable.Size = new System.Drawing.Size(543, 326);
+            this.MainTable.TabIndex = 0;
+            this.MainTable.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.MainTable.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.MainTable.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseClick);
+            // 
+            // Наименование
+            // 
+            this.Наименование.HeaderText = "Наименование";
+            this.Наименование.Name = "Наименование";
+            // 
+            // Цена
+            // 
+            this.Цена.HeaderText = "Цена";
+            this.Цена.Name = "Цена";
+            // 
+            // Количество
+            // 
+            this.Количество.HeaderText = "Количество";
+            this.Количество.Name = "Количество";
+            // 
+            // ф
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.NullValue = "+";
+            this.ф.DefaultCellStyle = dataGridViewCellStyle1;
+            this.ф.HeaderText = "ф";
+            this.ф.Name = "ф";
+            // 
+            // в
+            // 
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.NullValue = "-";
+            this.в.DefaultCellStyle = dataGridViewCellStyle2;
+            this.в.HeaderText = "в";
+            this.в.Name = "в";
+            this.в.Text = "";
+            // 
+            // productsBindingSource
+            // 
+            this.productsBindingSource.DataSource = typeof(Лаба1.Products);
+            this.productsBindingSource.RaiseListChangedEvents = global::Лаба1.Properties.Settings.Default.fdadfsasdf;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(300, 418);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 45);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Read from File to table";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(485, 418);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 45);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Serialize To File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // bindingNavigator1
+            // 
+            this.bindingNavigator1.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.bindingNavigator1.CountItem = this.bindingNavigatorCountItem;
+            this.bindingNavigator1.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.bindingNavigator1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem});
+            this.bindingNavigator1.Location = new System.Drawing.Point(0, 0);
+            this.bindingNavigator1.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.bindingNavigator1.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.bindingNavigator1.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.bindingNavigator1.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.bindingNavigator1.Name = "bindingNavigator1";
+            this.bindingNavigator1.PositionItem = this.bindingNavigatorPositionItem;
+            this.bindingNavigator1.Size = new System.Drawing.Size(709, 25);
+            this.bindingNavigator1.TabIndex = 3;
+            this.bindingNavigator1.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Добавить";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(43, 22);
+            this.bindingNavigatorCountItem.Text = "для {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Общее число элементов";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorDeleteItem.Text = "Удалить";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Переместить в начало";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Переместить назад";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Положение";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Текущее положение";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator1";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveNextItem.Text = "Переместить вперед";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveLastItem.Text = "Переместить в конец";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator2";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 386);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(235, 118);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(709, 531);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.bindingNavigator1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.MainTable);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.MainTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingNavigator1)).EndInit();
+            this.bindingNavigator1.ResumeLayout(false);
+            this.bindingNavigator1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView MainTable;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Наименование;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Цена;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Количество;
+        private System.Windows.Forms.DataGridViewButtonColumn ф;
+        private System.Windows.Forms.DataGridViewButtonColumn в;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.BindingSource productsBindingSource;
+        private System.Windows.Forms.BindingNavigator bindingNavigator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.TextBox textBox1;
+
+    }
+}
+
---
+++ Лаба1/Лаба1/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лаба1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            //dataGridView1.Rows.Clear();
+            if (e.ColumnIndex == 3)
+            {                                
+                MainTable.Rows[e.RowIndex].Cells[2].Value=Convert.ToInt32(MainTable.Rows[e.RowIndex].Cells[2].Value)+1;
+                MainTable.Refresh();                
+            }
+            else if (e.ColumnIndex == 4 && Convert.ToInt32(MainTable.Rows[e.RowIndex].Cells[2].Value) > 0)
+            {
+                MainTable.Rows[e.RowIndex].Cells[2].Value = Convert.ToInt32(MainTable.Rows[e.RowIndex].Cells[2].Value) - 1;
+                MainTable.Refresh();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Products prod = new Products();
+            prod=prod.ReadFromFile();
+            MainTable.DataSource = prod;
+            textBox1.Refresh();
+            for (int i = 0; i < 5; ++i)
+            {
+                textBox1.AppendText(prod.order[i].productname + prod.order[i].price);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string path=@"H:\Информатика\ООП\XML.xml";
+            using(System.IO.FileStream myFile = System.IO.File.Create(path))
+            {            
+                System.IO.StreamWriter file = new System.IO.StreamWriter(myFile);
+            
+                Products p = new Products();
+            
+                System.Xml.Serialization.XmlSerializer serializer = new System.Xml.Serialization.XmlSerializer(typeof(Products));
+                for (int i = 0; i < 11; ++i)
+                {
+                    Product prod = new Product();
+                    prod.productname = "Product" + Convert.ToString(i + 1);
+                    p.order.Add(prod);
+                    //p.order[i].productname = "Product" + Convert.ToString(i+1);//Почему меняет неправильно
+                }
+                serializer.Serialize(file, p);
+                //myFile.Close();
+                //НАдо закрывать поток прежде чем открыть его на чтение?
+                System.IO.StreamReader input = new System.IO.StreamReader(myFile);
+                serializer.Deserialize(input);
+            }
+            
+        }
+        /*private void Form1_Load(object sender, System.EventArgs e)
+        {
+            count += 1;
+        }*/
+
+        /*private void MainTable_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+
+        }*/
+    }
+    //Структура лучше ибо класс содержит ссылки а не значения полей. И сравнение 2-х классов будет сравнение ссылок а не значений
+    //Существует ли прямой доступ по индексу?
+
+    [System.Serializable]
+    public class Products
+    {
+        public Products()
+        {
+
+        }
+        public List<Product> order = new List<Product>();//Как сделать так чтобы сделать private, но использовать в Form
+        /*public Product this[int index]//Зачем это у Серёги?Или это в class Products?
+        {
+            get
+            {
+                return this.order[index];
+            }
+            set
+            {
+                this.order[index] = value;
+            }
+        }*/
+        public Products ReadFromFile()
+        {
+            string line;
+            System.IO.StreamReader file = new System.IO.StreamReader(@"H:\Информатика\ООП\XML.xml");
+            System.Xml.Serialization.XmlSerializer reader = new System.Xml.Serialization.XmlSerializer(typeof(Product));            
+            return (Products)reader.Deserialize(file);
+            
+            //Сделать заполнение dataGridView этими данными
+        }
+        
+        
+    }
+       
+    public class Product
+    {
+        //private string _name;//если делаем свойство без поля то можно не писать private string. И если просто присваивания и считывания значения то можно писать get;set. Если хоть одно расписываем то и второе надо в {}
+        public Product()
+        {
+            
+        }
+        public string productname//делать всё через get set
+        {
+            get;
+            set;
+        }
+        public double price
+        {
+            get;
+            set;
+        }
+        public int quantity
+        {
+            get;
+            set;
+        }
+        public double weight
+        {
+            get;
+            set;
+        }
+        public double dimensions
+        {
+            get;
+            set;
+        }
+        //
+        //Убрал для Сериализации
+        /*
+        public Product(string productname,string price="0", string quantity="0", string weight="0", string dimensions="")
+        {
+            this.productname = productname;
+            this.price = price;
+            this.quantity = quantity;
+            this.weight = weight;
+            this.dimensions = dimensions;
+        }//Разбираться в каком виде у меня в XML это будет. все string или double тоже. И как потом в DataGridView конвертировать double в string
+        
+        
+        public void FillDataGridView()
+        {
+            for (int i = 0; i < order.Count; ++i)
+            {
+                ;
+            }
+        }
+         */
+        /*public Order(string productname,double price, int quantity, double weight)
+        {
+            this.productname = productname;
+            this.price = price;
+            this.quantity = quantity;
+            this.weight = weight;
+        }*/
+    }
+    abstract class Calculation
+    {
+        abstract public double cost();
+    }
+    class RoadCalc : Calculation
+    {
+        const int N = 10;//Нужно ли вообще где-то const?
+        const double inf = 1e9;//Бесконечность для алгоритма
+        int[,] IncidenceMatrix = new int[N, N];//Тоже можно десериализовать из файла//Делать int или double?
+        int[,] tempMatr = new int[N, N];
+
+        protected void FillIncidenceMatrix()//Как сделать так чтобы в наследниках в итоге была одна и та же матрица смежности?(IncidenceMatrix)
+        {
+            Matrix.rand(ref IncidenceMatrix);
+        }
+        public override double cost(int x, int y)//Делать enum городов
+        {            
+            for (int k = 0; k < N; ++k)
+            {
+                Array.Copy(IncidenceMatrix, tempMatr, N * N);
+                for (int i = 0; i < N; ++i)
+                {
+                    for (int j = 0; j < N; ++j)
+                    {
+                        IncidenceMatrix[i, j] = Math.Min(tempMatr[i, j], tempMatr[i, k] + tempMatr[k, j]);
+                    }
+                }
+            }
+            return (double)IncidenceMatrix[x, y];
+        }
+    }
+    class AirCalc : Calculation
+    {       
+        //Делать enum городов                
+        public override double cost(double x,double y)
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+    }
+
+//Создать метод который создаст лист товаров и сделает прорисовку.
+/*public InfFile this[string key]
+        {
+            get
+            {
+                return this.DictionaryInfFile[key];
+            }
+            set
+            {
+                this.DictionaryInfFile[key] = value;
+            }
+        }*/
+
+    class Matrix
+    {
+        public static void rand(ref int[,] a)
+        {
+            Random rand1 = new Random();
+            for (int i=0;i<a.GetLength(0);++i){
+                for (int j = 0; j < a.GetLength(1); ++j)
+                {
+                    a[i, j] = rand1.Next(51);
+                }
+            }
+            
+        }
+
+    }
+}
